1d3d993dea6f90f1bb7273232ef4810f37aab836
Sudhan Regmi <xenonrazels@gmail.com>
2022-12-30 08:25:12 +0545

    adding media files

--- backend/main/settings.py
+++ backend/main/settings.py
@@ -123,6 +123,8 @@ STATIC_ROOT="/static/"
 STATIC_FILES_DIRS=[
     os.path.join(BASE_DIR,'static')
 ]
+MEDIA_URL = 'media/'
+MEDIA_ROOT=os.path.join(BASE_DIR,'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
